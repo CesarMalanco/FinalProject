ee5fae38ded4f512716e8d682ea798e9613da242
Danna Castro <dannacristinacastrohdz@gmail.com>
2024-12-08 01:19:39 -0600

    Proyecto parte admin1de WinForms

---
+++ Frm_Agregar.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsFinalProyect
+{
+    public partial class Frm_Agregar : Form
+    {
+        private bool isOpening; // Para controlar si se está abriendo o cerrando el panel
+
+        public Frm_Agregar()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Agregar_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 10; //  mayor velocidad
+            PnlAddData.Width = 0; // El panel comienza cerrado
+            PnlAddData.Visible = false;
+            Contenedor_Peliculas.Width = 819; // Ancho total inicial del grid
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int panelSpeed = 30; // Velocidad de cambio del ancho del panel
+            int gridSpeed = 30;  // Velocidad de ajuste del ancho del grid (coherente con panelSpeed)
+
+            if (isOpening)
+            {
+                // Abrir el panel
+                if (PnlAddData.Width < 300)
+                {
+                    PnlAddData.Width += panelSpeed; // Incrementar el ancho del panel
+                    Contenedor_Peliculas.Width -= gridSpeed; // Reducir gradualmente el ancho del grid
+                }
+                else
+                {
+                    PnlAddData.Width = 300; // Fijar el tamaño final del panel
+                    Contenedor_Peliculas.Width = 513; // Fijar el tamaño final del grid
+                    timer1.Stop(); // Detener la animación
+                }
+            }
+            else
+            {
+                // Cerrar el panel
+                if (PnlAddData.Width > 0)
+                {
+                    PnlAddData.Width -= panelSpeed; // Reducir gradualmente el ancho del panel
+                    Contenedor_Peliculas.Width += gridSpeed; // Incrementar gradualmente el ancho del grid
+                }
+                else
+                {
+                    PnlAddData.Width = 0; // Fijar el tamaño final del panel
+                    Contenedor_Peliculas.Width = 814; // Fijar el tamaño final del grid
+                    PnlAddData.Visible = false; // Ocultar el panel al terminar
+                    timer1.Stop(); // Detener la animación
+                }
+            }
+        }
+
+
+
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            isOpening = false;
+            timer1.Start();
+        }
+
+        private void Btn_Agregar_Click(object sender, EventArgs e)
+        {
+            isOpening = true;
+            PnlAddData.Visible = true; // Asegurarse de que el panel sea visible
+            timer1.Start();
+        }
+
+
+        private void Btn_Delete_Click(object sender, EventArgs e)
+        {
+            Pic_photo.Image = null;
+        }
+
+        private void Btn_Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Pic_photo.ImageLocation = openFileDialog.FileName;
+            }
+        }
+
+        private void Btn_Close_Page_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
---
+++ Frm_Productos.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WinFormsFinalProyect
+{
+    partial class Frm_Productos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label4 = new Label();
+            Btn_Close_Page = new PictureBox();
+            Contenedor_Peliculas = new FlowLayoutPanel();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)Btn_Close_Page).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 90F));
+            tableLayoutPanel1.Controls.Add(label4, 0, 0);
+            tableLayoutPanel1.Controls.Add(Btn_Close_Page, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Top;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.Padding = new Padding(2);
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(829, 60);
+            tableLayoutPanel1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.Dock = DockStyle.Left;
+            label4.Font = new Font("Microsoft YaHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = SystemColors.ActiveCaptionText;
+            label4.Location = new Point(5, 2);
+            label4.Name = "label4";
+            label4.Size = new Size(332, 56);
+            label4.TabIndex = 5;
+            label4.Text = "Peliculas Disponibles";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Btn_Close_Page
+            // 
+            Btn_Close_Page.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            Btn_Close_Page.BackColor = Color.Transparent;
+            Btn_Close_Page.Image = Properties.Resources.icon_ejemplo;
+            Btn_Close_Page.Location = new Point(779, 5);
+            Btn_Close_Page.Name = "Btn_Close_Page";
+            Btn_Close_Page.Size = new Size(45, 43);
+            Btn_Close_Page.SizeMode = PictureBoxSizeMode.Zoom;
+            Btn_Close_Page.TabIndex = 19;
+            Btn_Close_Page.TabStop = false;
+            Btn_Close_Page.Click += Btn_Close_Page_Click;
+            // 
+            // Contenedor_Peliculas
+            // 
+            Contenedor_Peliculas.BackColor = SystemColors.Info;
+            Contenedor_Peliculas.Location = new Point(56, 77);
+            Contenedor_Peliculas.Name = "Contenedor_Peliculas";
+            Contenedor_Peliculas.Size = new Size(716, 511);
+            Contenedor_Peliculas.TabIndex = 9;
+            // 
+            // Frm_Productos
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(829, 600);
+            Controls.Add(Contenedor_Peliculas);
+            Controls.Add(tableLayoutPanel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Frm_Productos";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Frm_Productos";
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)Btn_Close_Page).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox Btn_Close_Page;
+        private FlowLayoutPanel Contenedor_Peliculas;
+        private Label label4;
+    }
+}
